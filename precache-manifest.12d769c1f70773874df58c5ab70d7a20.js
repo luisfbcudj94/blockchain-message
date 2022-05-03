6498c24212bf8246ab45665e9b818c110d9cbadc
Luis Alejandro Pérez Abril <alejandro.perez@mvm.com.co>
2022-05-03 14:48:29 -0500

    Updates

---
+++ precache-manifest.12d769c1f70773874df58c5ab70d7a20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64938d818330fc7cd1019e46bd2626fe",
+    "url": "/luisfbcudj94/blockchain-message/index.html"
+  },
+  {
+    "revision": "9444587f62a2197908d5",
+    "url": "/luisfbcudj94/blockchain-message/static/css/main.0c1e2795.chunk.css"
+  },
+  {
+    "revision": "a7ea378abc5a857845a7",
+    "url": "/luisfbcudj94/blockchain-message/static/js/2.0700586d.chunk.js"
+  },
+  {
+    "revision": "9444587f62a2197908d5",
+    "url": "/luisfbcudj94/blockchain-message/static/js/main.f5079614.chunk.js"
+  },
+  {
+    "revision": "8a5070201c8b2a583139",
+    "url": "/luisfbcudj94/blockchain-message/static/js/runtime-main.f1b31c54.js"
+  }
+]);
